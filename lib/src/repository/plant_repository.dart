6366c92cb56d971f7560d2cf299c935fcdcd8470
Ruben <schaefer-ruben@web.de>
2020-08-16 10:25:11 +0200

    created myplant functions

--- lib/src/repository/plant_repository.dart
+++ lib/src/repository/plant_repository.dart
@@ -4,6 +4,10 @@ import '../model/plants.dart';
 abstract class PlantRepository {
   Future<Plant> fetchPlant(String name);
   Future<List<Plant>> fetchAllPlants();
+
+  Future<List<Plant>> fetchAllMyPlants();
+  Future<Plant> fetchMyPlant(String name);
+  void addToMyPlants(Plant plant);
 }
 
 class FakePlantRepository implements PlantRepository{
@@ -27,6 +31,26 @@ class FakePlantRepository implements PlantRepository{
     });
 
     }
+    @override
+    Future<List<Plant>> fetchAllMyPlants(){
+      return Future.delayed(Duration(seconds: 0), () {
+        return DummyData().myDummyPlants;
+      });
+    }
+
+    @override
+    Future<Plant> fetchMyPlant(String name){
+      return Future.delayed(Duration(seconds: 0), () {
+      //return first "fetched" plant eventually add an id so name can be doubled.
+      return DummyData().myDummyPlants.where((element) => element.name == name).first;
+      },
+      );
+    }
+
+    @override
+    void addToMyPlants(Plant plant) {
+    DummyData().myDummyPlants.add(plant);
+  }
 }
 
 class NetworkException implements Exception{}
@@ -44,5 +68,12 @@ class DummyData{
     Plant(name: "Arbol", waterValue: 6),
     Plant(name: "Conanstiaza", waterValue: 12),
 
+  ];
+
+
+  List<Plant> myDummyPlants = [
+    Plant(name: "Cactus", waterValue: 7),
+    Plant(name: "Arbol", waterValue: 6),
+    Plant(name: "Conanstiaza", waterValue: 12),
   ];
 }
